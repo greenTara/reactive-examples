20e30d2815e603099c9e827e6a1bb383f72f5810
Roshan Mathews <rmathews@gmail.com>
2013-12-10 22:42:45 +0530

    Ignoring (and removing) .worksheet directory created by Eclipse.

--- .worksheet/src/observable.obs21.scala
+++
@@ -1,5 +0,0 @@
-package observable
-
-object obs21 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(78); 
-  println("Welcome to the Scala worksheet")}
-}
